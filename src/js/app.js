b12a5e4e978ea2306c2b1db49f4c639a5cc01975
syart7 <syartibrahimi@gmail.com>
2022-03-02 19:42:27 +0100

    inital commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,17 @@ require('../scss/style.scss');
 
 /********** Paste your code here! ************/
 
+
+$('.scene').each((index, element) => {
+    new Parallax(element)
+  })
+
+
+$(window).on('scroll',()=>{
+const scroll = $(window).scrollTop()
+    if(scroll>0){
+      $('header').addClass('isScrolling')
+    }else{
+      $('header').removeClass('isScrolling')
+    }
+  })
